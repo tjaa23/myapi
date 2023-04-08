f2bb58c9e507d9ec2a3b898b4a9c3b57113615ec
tjaa <morita23.scarecrow@gmail.com>
2023-04-08 12:08:56 +0900

    Update main.js

--- ビジョン _ 株式会社サイバーエージェント_files/main.js
+++ ビジョン _ 株式会社サイバーエージェント_files/main.js
@@ -1,5 +1,11 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
+$(function(){
+	setInterval(function(){
+		console.log('テスト')
+	},2000);
+});
+
 /**
  * jQueryオブジェクトの拡張
  *
